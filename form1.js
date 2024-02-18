7776b6e08f734887a9298d9fdc6696209f71fe07
Aditya Agre <adityaagrecollege@gmail.com>
2024-02-18 11:57:07 +0530

    api1

---
+++ form1.js
@@ -0,0 +1,13 @@
+function js_valid_func()
+{
+    var u_name = document.form1.uname;
+    var u_rn = document.form1.urn;
+    var u_dept = document.form1.udept;
+    var u_year = document.form1.uyear;
+    var u_msex = document.form1.msex;
+    var u_fsex = document.form1.fsex;
+    var u_phone = document.form1.uphone;
+    var u_mail = document.form1.umail;
+    var u_exp = document.form1.uexp;
+}
+
